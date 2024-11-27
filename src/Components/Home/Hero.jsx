64e5bfb7ce0c01984acfccb39433d2e6d6bf7fe8
NMAtech <nadindramaulanaaziz@gmail.com>
2024-11-27 14:09:32 +0700

    style: complete adding hero on homepage

---
+++ src/Components/Home/Hero.jsx
@@ -0,0 +1,24 @@
+import imgHero from "../../assets/img/bg-hero.jpg";
+import { MdLeaderboard } from "react-icons/md";
+
+const Hero = () => {
+  return (
+    <div className="relative">
+      <div className="w-full">
+        <img
+          src={imgHero}
+          alt="Image hero"
+          className="w-full h-[300px] md:h-[400px] object-cover bg-center bg-no-repeat"
+        />
+      </div>
+      <div className="absolute inset-0 bg-main-primary bg-opacity-50 flex justify-center items-center gap-3">
+        <MdLeaderboard className="text-main-accent text-[30px] md:text-[40px]" />
+        <h1 className="text-white text-[30px] md:text-[40px] font-semibold">
+          Country <span className="text-main-accent">Rank</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
